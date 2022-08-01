fd83d3758a0846ac073df35fb76a595c268f6a43
diego-garro <diego.garromolina@yahoo.com>
2022-08-01 13:28:04 -0600

    feat: postcss config

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,10 +3,17 @@
 <template>
   <nav class="navbar bg-gray-800 py-3">
     <div class="container mx-auto flex text-white">
-      <button class="border border-white rounded-md text-white px-2 mx-1">
+      <button>
         <font-awesome-icon icon="fa-solid fa-bars" />
       </button>
       Menú
     </div>
   </nav>
 </template>
+
+<style scoped lang="postcss">
+button {
+  @apply border border-white rounded-md;
+  @apply text-white px-2 mx-1;
+}
+</style>
