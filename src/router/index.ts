cc85ef128c75f2778770cf9fc8ae21c18f531ebd
diego-garro <diego.garromolina@yahoo.com>
2022-08-01 09:26:37 -0600

    Initial commit for v2.0.0

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
