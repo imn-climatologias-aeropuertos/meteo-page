678ec3d085389f65e16a8d3f12f317add934b3d6
diego-garro <diego.garromolina@yahoo.com>
2022-08-05 17:13:58 -0600

    add all necesary icons from font awesome

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,13 +1,13 @@
+<script setup lang="ts">
+import links from "../static/menu_links.json";
+</script>
+
 <template>
   <ul>
-    <li>
-      <router-link :to="{ name: 'home' }" class="block">
-        <font-awesome-icon icon="fa-solid fa-house" class="px-2" /> Inicio
-      </router-link>
-    </li>
-    <li>
-      <router-link :to="{ name: 'about' }" class="block">
-        <font-awesome-icon icon="fa-solid fa-eject" class="px-2" /> About
+    <li v-for="link in links" :key="link.icon">
+      <router-link :to="{ name: link.name }" class="block">
+        <font-awesome-icon :icon="`fa-solid fa-${link.icon}`" class="px-2" />
+        {{ link.text }}
       </router-link>
     </li>
   </ul>
@@ -16,6 +16,7 @@
 <style scoped lang="postcss">
 li {
   @apply border border-gray-800 rounded-md;
-  @apply px-1 m-3;
+  @apply px-1 m-3 text-gray-800;
+  @apply hover:bg-gray-800 hover:text-white;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHouse, faBars, faEject, faE } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faBars, faEject, faVolcano, faRecycle } from '@fortawesome/free-solid-svg-icons'
+import { faSatellite, faDatabase, faHurricane, faClipboard } from '@fortawesome/free-solid-svg-icons'
+import { faWaveSquare, faCloudSunRain, faGlobeAmericas } from '@fortawesome/free-solid-svg-icons'
+import { faTemperatureLow, faChartLine, faChartArea, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPlaneDeparture, faWater, faFireFlameCurved } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faHouse, faBars, faEject)
+library.add(faHouse, faBars, faEject, faSatellite, faDatabase, faHurricane)
+library.add(faWaveSquare, faCloudSunRain, faGlobeAmericas, faTemperatureLow)
+library.add(faChartLine, faChartArea, faPlaneDeparture, faWater, faFireFlameCurved)
+library.add(faVolcano, faRecycle, faClipboard, faEnvelope)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
